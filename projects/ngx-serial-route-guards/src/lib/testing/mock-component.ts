4f3252c4e1520f7f937f8c3e7cc31956502e78fb
tylerwigington <tyler.wigington.io@gmail.com>
2025-04-23 08:50:11 -0500

    Upgrade to angular 19

--- projects/ngx-serial-route-guards/src/lib/testing/mock-component.ts
+++ projects/ngx-serial-route-guards/src/lib/testing/mock-component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mock-component',
-  template: '<p>test</p>',
+    selector: 'mock-component',
+    template: '<p>test</p>',
+    standalone: false
 })
 export class MockComponent extends Component {}
